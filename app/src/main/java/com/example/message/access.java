713e57e440700b258675afdee44731f4e1ab83ec
Raghav <memaniraghavdecember12@gmail.com>
2023-04-06 12:54:30 +0530

    Second Commit

--- app/src/main/java/com/example/message/access.java
+++ app/src/main/java/com/example/message/access.java
@@ -9,26 +9,64 @@ import android.content.pm.PackageManager;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class access extends AccessibilityService {
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
     AccessibilityServiceInfo info= new AccessibilityServiceInfo();
     private static final String TAG="MY_SERVICE";
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
-        Log.e(TAG,"DONE");
+        Log.e(TAG, "DONE");
 
 
-        String packname=  event.getPackageName().toString();
-        PackageManager pkg=this.getPackageManager();
-        Log.d(TAG,"YOUR RESULTS:"+pkg);
+        String packname = event.getPackageName().toString();
+        PackageManager pkg = this.getPackageManager();
+        Log.d(TAG, "YOUR RESULTS:" + pkg);
+        // Create a new user with a first and last name
+
         try {
-            ApplicationInfo applicationInfo= pkg.getApplicationInfo(packname,0);
-             CharSequence chg= pkg.getApplicationLabel(applicationInfo);
-            Log.d(TAG, (String) chg);
-            //Log.d(TAG,"DONE1"+applicationInfo);
-            System.out.println(chg);
+            ApplicationInfo applicationInfo = pkg.getApplicationInfo(packname, 0);
+            CharSequence chg = pkg.getApplicationLabel(applicationInfo);
+            Map<String, Object> user = new HashMap<>();
 
+            if (!user.toString().equals("System UI") || !user.toString().equals("System launcher") || !user.toString().equals("System launcher")) {
+
+
+                user.put("first", chg);
+
+//        user.put("first", "Ada");
+//        user.put("last", "Lovelace");
+//        user.put("born", 1815);
+                db.collection("users")
+                        .add(user)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG, "Error adding document", e);
+                            }
+                        });
+
+                Log.d(TAG, (String) chg);
+                //Log.d(TAG,"DONE1"+applicationInfo);
+                System.out.println(chg);
+
+            }
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
             System.out.println(e.toString());
